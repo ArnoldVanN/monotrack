6d30ef2b902df5bab45e3642dd7dbc1b7a07b78d
ArnoldVanN <arnovannieuwenhuyzen@outlook.com>
2026-01-06 00:33:02 +0100

    fix: move code using github event back to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,29 +7,6 @@ HEAD=${3:-}
 ARGS=${4:-}
 CONFIG=${5:-}
 
-# Determine base and head SHAs if not provided
-if [ -z "$BASE" ] || [ -z "$HEAD" ]; then
-  case "$GITHUB_EVENT_NAME" in
-    push)
-      BASE="${BASE:-$(jq -r '.before // empty' < "$GITHUB_EVENT_PATH")}"
-      HEAD="${HEAD:-$(jq -r '.after // empty' < "$GITHUB_EVENT_PATH")}"
-      ;;
-    pull_request)
-      BASE="${BASE:-$(jq -r '.pull_request.base.sha' < "$GITHUB_EVENT_PATH")}"
-      HEAD="${HEAD:-$(jq -r '.pull_request.head.sha' < "$GITHUB_EVENT_PATH")}"
-      ;;
-    workflow_dispatch|schedule)
-      # User must provide explicitly
-      ;;
-  esac
-fi
-
-HEAD="${HEAD:-$GITHUB_SHA}"
-
-if [ -z "$BASE" ]; then
-  echo "::warning::Unable to determine base SHA."
-fi
-
 # Build command
 CMD=(monotrack "$COMMAND")
 if [ -n "$ARGS" ]; then
