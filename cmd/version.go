491bbaf1f46a86e0f239e12bf59d119397b60745
ArnoldVanN <arnovannieuwenhuyzen@outlook.com>
2026-01-07 23:02:23 +0100

    fix: im blind

--- cmd/version.go
+++ cmd/version.go
@@ -14,6 +14,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Monotrack",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Monotrack versioning tool %v %v\n", version, date)
+		fmt.Printf("v%v\n %v\n", version, date)
 	},
 }
